4d5f39bf6849fe0663f52e3c2209c2a2074a0ce9
manhpt <manhhailua@gmail.com>
2023-02-19 18:28:07 +0700

    feat: add a snackbar of error

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import { useSelector } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
-import { CssBaseline, StyledEngineProvider } from "@mui/material";
+import { Alert, CssBaseline, Snackbar, StyledEngineProvider } from "@mui/material";
 import Routes from "routes";
 import themes from "themes";
 import NavigationScroll from "layout/NavigationScroll";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { openAI } from "utils/axios";
 
 const App = () => {
   const customization = useSelector((state) => state.customization);
   const apiKey = useSelector((state) => state.user?.openAI?.apiKey);
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     openAI.interceptors.request.use((config) => {
       config.headers["Authorization"] = `Bearer ${apiKey}`;
       return config;
+    }, (error) => {
+      return Promise.reject(error);
+    });
+
+    openAI.interceptors.response.use((response) => {
+      return response;
+    }, (error) => {
+      setError(error);
+      setOpen(true);
+      return Promise.reject(error);
     });
   }, [apiKey]);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <StyledEngineProvider injectFirst>
@@ -24,6 +40,11 @@ const App = () => {
         <CssBaseline />
         <NavigationScroll>
           <Routes />
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+              {error?.message}
+            </Alert>
+          </Snackbar>
         </NavigationScroll>
       </ThemeProvider>
     </StyledEngineProvider>
